ac3ddf9239ff9ead8008c61f38603e03a17c16dc
Chayandev Bera <chayandevbera@gmail.com>
2023-09-07 21:28:54 +0530

    third commit

--- app/src/main/java/com/example/cloudcounselagecompass/views/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/cloudcounselagecompass/views/fragments/HomeFragment.kt
@@ -13,7 +13,6 @@ import com.example.cloudcounselagecompass.databinding.FragmentHomeBinding
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
-
 /**
  * A simple [Fragment] subclass.
  * Use the [HomeFragment.newInstance] factory method to
